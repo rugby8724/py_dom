17e29817802f3e9ec3067370fb4ebef03b8ba553
Jeremiah <jeremiah.d.wise@gmail.com>
2021-06-26 15:14:23 -0500

    rename proj file

--- dominion-proj/wsgi.py
+++ dominion-proj/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for dominion project.
+WSGI config for dominion-proj project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dominion.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dominion-proj.settings')
 
 application = get_wsgi_application()
